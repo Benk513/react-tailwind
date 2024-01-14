3fe31d30caed55336bf49e152f70190510545ce7
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-01-14 18:03:18 +0100

    i have added the Review Card component to the Customer Review parent component

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,26 @@
+import { star } from "../assets/icons"
+
+const ReviewCard = ({imgURL,customerName,rating,feedback}) => {
+  return (
+      <div className="flex justify-center items-center flex-col">
+          <img
+              src={imgURL}
+              alt="customer"
+              className="rounded-full object-cover w-[120px] h-[120px]" />
+          <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+          <div className="mt-3 flex justify-center items-center gap-2.5">
+              <img
+                  src={star}
+                  alt="icon"
+                  width={24}
+                  height={24}
+                  className="object-contain m-0"
+              />
+              <p>({rating})</p>
+          </div>
+          <h3>{customerName}</h3>
+    </div>
+  )
+}
+
+export default ReviewCard
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,23 @@
+import { reviews } from "../constant"
+import ReviewCard from "../components/ReviewCard"
+
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red">Customers </span>Say ?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, debitis ducimus vero officiis incidunt accusamus aperiam, tempore eius eum a.</p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">{reviews.map((reviews) => (
+        <ReviewCard key={reviews.customerName}
+          imgURL={reviews.imgURL}
+          customerName={reviews.customerName}
+          rating={reviews.rating}
+          feedback={reviews.feedback}
+        />
+      ))} </div>
+    </section>
   )
 }
 
